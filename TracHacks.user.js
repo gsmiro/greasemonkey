2b5eb84b440eb52d5aaf8f47b8bb56c19058295c
Gabriel Serralha Miró <gsmiro@gmail.com>
2014-08-05 16:17:20 -0300

    Panel factory working

--- TracHacks.user.js
+++ TracHacks.user.js
@@ -7,7 +7,30 @@
 // ==/UserScript==
 
 console.log('Runnning track hacks!');
+document.onreadystatechange = function(){
+    console.log(document.readyState)
+}
 (function(document,$){
+    console.log('ini')
+    var _cfg = {
+              'query':{
+                  'post':function(path,cfg,doc,$){
+                        var el = doc.getElementById('filters');
+                        if(el)el.setAttribute('class','');
+                    },
+              },
+             'roadmap':{
+                 'queryParams':'',
+                 'post':function(path,cfg,doc,$){
+                     $('span.first.interval > a').map(function(idx,item,arr){
+                         item.href = cfg.queryParams || item.href;
+                         return item;
+                     });
+                  }        
+              }
+          };
+    
+    
     function readpanels($,cfg){
         var ret = {};
         var panels = $('div.nav#mainnav > ul > li > a').map(function(idx,item,arr){
@@ -23,43 +46,36 @@ console.log('Runnning track hacks!');
         
     }
     
-    function readcfg(db,ctx){
-        var nm = 'tracclientcfg';
-        return ctx && ctx[nm] && db.val(nm,ctx[nm]) || db.val(nm) || db.val(nm,
-          {
-              'query':{
-                  'post':function(path,cfg,doc,$){
-                        var el = doc.getElementById('filters');
-                        if(el)el.setAttribute('class','');
-                    },
-              },
-             'roadmap':{
-                 'queryParams':'',
-                 'post':function(path,cfg,doc,$){
-                         $('span.first.interval > a').map(function(idx,item,arr){
-                         item.href = cfg.queryParams || item.href;
-                         return item;
-                     });
-                  }        
-              }
-          });
+    function readcfg(nm,ctx){
+        console.log('readcfg ' );
+        return ctx && ctx[nm] || _cfg;
     }
     
     function ready(path,procs,document,$){
+        console.log('ready');
         var pttrn = path.match('\\w+$');
+        console.log(pttrn);
         pttrn = pttrn && pttrn.length && pttrn[0];
+        console.log(procs[pttrn].post)
         if(pttrn && procs[pttrn].post){
             console.log('Triggered '+pttrn + ' for ' +path);
             return procs[pttrn].post(path,procs[pttrn],document,$);
         }
     };
     
+    console.log(Storage)
+    
+    Storage.prototype._fs = {};
     Storage.prototype.val = function(k,v){
+        if(typeof v === 'function'){
+            this._fs[k] = v;
+            return v;
+        }
         if(v === undefined){
             try{
-                return JSON.parse(this.getItem(k))
+                return JSON.parse(this.getItem(k)) || this._fs[k];
             }catch(e){
-                return this.getItem(k);
+                return this.getItem(k) || this._fs[k];
             }
         }else{
             try{
@@ -72,10 +88,9 @@ console.log('Runnning track hacks!');
             }
         };
     }
-    
     document.onreadystatechange = function(state){
          if(document.readyState === 'complete'){
-           var cfg = readcfg(localStorage,this);
+           var cfg = readcfg('tracclientcfg',this);
            ready(location.pathname,cfg,document,jQuery);
          }
     }
